4b0f85458248e5486ea781a6acca932d49db590c
ossogames <79472292+ossogames@users.noreply.github.com>
2022-10-01 18:32:13 -0300

    test

---
+++ OS Scanner/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Resources;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// As informações gerais sobre um assembly são controladas por
+// conjunto de atributos. Altere estes valores de atributo para modificar as informações
+// associada a um assembly.
+[assembly: AssemblyTitle("OS Scanner")]
+[assembly: AssemblyDescription("Minecraft Cheat Scanner")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("OS Technologies")]
+[assembly: AssemblyProduct("OS Scanner")]
+[assembly: AssemblyCopyright("Copyright © 2021 - OS Technologies")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Definir ComVisible como false torna invisíveis os tipos neste assembly
+// para componentes COM.  Caso precise acessar um tipo neste assembly a partir de 
+// COM, defina o atributo ComVisible como true nesse tipo.
+[assembly: ComVisible(false)]
+
+// O GUID a seguir será destinado à ID de typelib se este projeto for exposto para COM
+[assembly: Guid("dabf01a3-d228-4e27-a05c-850af327bcf1")]
+
+// As informações da versão de um assembly consistem nos quatro valores a seguir:
+//
+//      Versão Principal
+//      Versão Secundária 
+//      Número da Versão
+//      Revisão
+//
+[assembly: AssemblyVersion("10.6.0.0")]
+[assembly: AssemblyFileVersion("10.6.0.0")]
+[assembly: NeutralResourcesLanguage("pt-BR")]
